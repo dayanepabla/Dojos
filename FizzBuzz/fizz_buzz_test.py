2fb1fe82ed2caf3e3fd2757a63ff2538e037331f
Dayane Pabla <dayanepabla@gmail.com>
2014-09-22 20:07:23 -0300

    Primeiro teste

--- FizzBuzz/fizz_buzz_test.py
+++ FizzBuzz/fizz_buzz_test.py
@@ -3,7 +3,7 @@ from fizz_buzz import FizzBuzz
 
 class FizzBuzzTest(unittest.TestCase):
     def test_number(self):
-        self.number(FizzBuzz(1),1)
+        self.assertEqual(FizzBuzz(1),1)
 
 if __name__ == '__main__':
     unittest.main()
