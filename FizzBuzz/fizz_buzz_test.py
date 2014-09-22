376746475506e155fa08352b11b33c829ac60c3c
Dayane Pabla <dayanepabla@gmail.com>
2014-09-22 19:59:02 -0300

    Primeiro teste

--- FizzBuzz/fizz_buzz_test.py
+++ FizzBuzz/fizz_buzz_test.py
@@ -1,4 +1,9 @@
 import unittest
+from fizz_buzz import FizzBuzz
 
 class FizzBuzzTest(unittest.TestCase):
-    def test_fizz(self):
+    def test_number(self):
+        self.number(FizzBuzz(1),1)
+
+if __name__ == '__main__':
+    unittest.main()
