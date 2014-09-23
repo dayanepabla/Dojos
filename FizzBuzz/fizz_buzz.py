6eb0cc8e2aec08e8f7116e16afaa214ee71c6c3a
Dayane Pabla <dayanepabla@gmail.com>
2014-09-22 22:14:33 -0300

    Fim do programa

--- FizzBuzz/fizz_buzz.py
+++ FizzBuzz/fizz_buzz.py
@@ -1,2 +1,15 @@
 def fizz_buzz(number):
-    return 5
+
+    if number % 15 == 0:
+        return "fizzbuzz"
+
+    if number % 3 == 0:
+        return "fizz"
+
+    if number % 5 == 0:
+        return "buzz"
+
+    return number
+
+for number in range(1,100):
+    print (fizz_buzz(number))
--- FizzBuzz/fizz_buzz_test.py
+++ FizzBuzz/fizz_buzz_test.py
@@ -11,6 +11,14 @@ class FizzBuzzTest(unittest.TestCase):
     def test_fizz_buzz_four(self):
         self.assertEqual(fizz_buzz(4), 4)
 
+    def test_multiple_three(self):
+        self.assertEqual(fizz_buzz(3), "fizz")
+
+    def test_multiple_five(self):
+        self.assertEqual(fizz_buzz(5), "buzz")
+
+    def test_multiple_three_and_fiva(self):
+        self.assertEqual(fizz_buzz(15), "fizzbuzz")
 
 if __name__ == '__main__':
     unittest.main()
